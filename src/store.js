e3813797abc5d27c68d5b4c3552fe2320be08bda
esojlm5 <ne.ver1@outlook.com>
2022-09-01 16:03:09 -0500

    fixes error on deploy

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '@/redux/counter/counterSlice'
-import userReducer from '@/redux/User/userSlice'
+import counterReducer from '@/redux/Counter/CounterSlice'
+import userReducer from '@/redux/User/UserSlice'
 
 export const store = configureStore({
   reducer: {
